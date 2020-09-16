cb28f4f0529e7224bde0aabde71cb6de41099a70
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-16 23:16:57 +0300

    добавляет решение 5 задачи

--- js/task-5.js
+++ js/task-5.js
@@ -1,4 +1,4 @@
-const countryName = "Австралия";
+const countryName = "яМайКа";
 
 const CANCELED_BY_USER = "Отменено пользователем!";
 const NO_DELIVERY = "В выбранную страну доставка недоступна.";
@@ -13,12 +13,34 @@ if (countryName === null) {
   message = CANCELED_BY_USER;
 } else {
   country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
+  switch (country) {
+    case CHINA:
+      country === "Китай";
+      break;
+    case AUSTRALIA:
+      country === "Австралия";
+      break;
+    case INDIA:
+      country === "Индия";
+      break;
+    case JAMAICA:
+      country === "Ямайка";
+      break;
+    default:
+      message = NO_DELIVERY;
+  }
 }
-
-// }
-// if () {
-//     message = `Доставка в ${country} будет стоить ${price} кредитов`;
-// }
-
-console.log(country);
+if (country === "Китай") {
+  price = 100;
+  message = `Доставка в ${CHINA} будет стоить ${price} кредитов`;
+} else if (country === "Австралия") {
+  price = 170;
+  message = `Доставка в ${AUSTRALIA} будет стоить ${price} кредитов`;
+} else if (country === "Индия") {
+  price = 80;
+  message = `Доставка в ${INDIA} будет стоить ${price} кредитов`;
+} else if (country === "Ямайка") {
+  price = 120;
+  message = `Доставка в ${JAMAICA} будет стоить ${price} кредитов`;
+}
 console.log(message);
