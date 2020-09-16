7ebb0882828a7b8136a59f0554c38b1c575bb421
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-16 16:18:59 +0300

    решает 3-ю задачу

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+// объявить переменные для полей паролей, а так же выводимых сообщений
+const userPassword = "jqueryismyjam";
+
+const ADMIN_PASSWORD = "jqueryismyjam";
+
+const CANCELED_BY_USER = "Отменено пользователем!";
+const ACCESS_IS_ALLOWED = "Добро пожаловать!";
+const ACCESS_DENIED = "Доступ запрещен, неверный пароль!";
+const ACCESS_DENIE = "неверный пароль!";
+
+let message = `Добро пожаловать!`;
+if (userPassword === null) {
+  message = CANCELED_BY_USER;
+} else if (userPassword !== ADMIN_PASSWORD) {
+  message = ACCESS_DENIED;
+}
+
+console.log(message);
